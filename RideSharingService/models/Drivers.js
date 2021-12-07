672d77a7ef4a2806b95fa1d1e6841687463e7c5e
Mehedi-909 <bsse0909@iit.du.ac.bd>
2021-12-08 00:24:33 +0600

    Step4

---
+++ RideSharingService/models/Drivers.js
@@ -0,0 +1,46 @@
+const drivers = [
+    {
+        id: 1,
+        name: "Driver1",
+        car: "Car1",
+        currentX: 9,
+        currentY: 7,
+    },
+    {
+        id: 2,
+        name: "Driver2",
+        car: "Car2",
+        currentX: 45,
+        currentY: 32,
+    },
+    {
+        id: 3,
+        name: "Driver3",
+        car: "Car3",
+        currentX: 17,
+        currentY: 63,
+    },
+    {
+        id: 4,
+        name: "Driver4",
+        car: "Car4",
+        currentX: 11,
+        currentY: 12,
+    },
+    {
+        id: 5,
+        name: "Driver5",
+        car: "Car5",
+        currentX: 78,
+        currentY: 91,
+    },
+    {
+        id: 6,
+        name: "Driver6",
+        car: "Car6",
+        currentX: 15,
+        currentY: 31,
+    }
+]
+
+module.exports = drivers;
